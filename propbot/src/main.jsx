ea21377250b28f501069dae47577864ddae8e54e
amitdervadiya <amitdervadiya@gmail.com>
2025-09-09 18:04:51 +0530

    assign

--- propbot/src/main.jsx
+++ propbot/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/Store.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
